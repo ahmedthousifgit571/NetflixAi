957087dbcba81f55aad99e2f77c2f9ca5cdd9134
ahmedthousifgit571 <ahmedthousif571@gmail.com>
2025-04-23 12:51:41 +0530

    firebase setup

---
+++ src/utils/validate.js
@@ -0,0 +1,18 @@
+const validateData =(email,password)=>{
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    const isValidName = nameRegex.test(name.trim());
+    console.log(nameRegex.test("ahmed thousif")); 
+    
+    const isValidEmail = emailRegex.test(email)
+    const isValidePassword = passwordRegex.test(password)
+
+    if(!isValidEmail) return "Email Id is not valid"
+    if(!isValidePassword) return "Password is not valid"
+
+
+    return null
+}
+
+export default validateData
