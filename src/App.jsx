e8ef474bc7012ce356be1e768fb3a832a56176a5
ahmedthousifgit571 <ahmedthousif571@gmail.com>
2025-04-24 14:40:57 +0530

    signout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import './index.css'
 import Body from './components/Body'
 import { RouterProvider } from 'react-router-dom'
 import appRouter from './routes/router'
+import {Provider} from 'react-redux'
+import store from './utils/store'
 
 function App() {
 
 
   return (
-    <>
+    <Provider store={store}>
      <RouterProvider router={appRouter} />
-    </>
+    </Provider>
   )
 }
 
---
+++ src/routes/AuthWrapper.jsx
@@ -0,0 +1,34 @@
+// components/AuthWrapper.jsx
+import { useEffect } from 'react'
+import { auth } from '../utils/firebase'
+import { onAuthStateChanged } from "firebase/auth"
+import { useDispatch } from 'react-redux'
+import { addUsers, removeUsers } from '../utils/userSlice'
+import { useNavigate } from 'react-router-dom'
+
+const AuthWrapper = ({ children }) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const {uid, email, displayName} = user
+                dispatch(addUsers({uid, email, displayName}))
+                
+                
+            } else {
+                dispatch(removeUsers())
+                navigate('/')
+            }
+        })
+
+        // Clean up subscription
+        return () => unsubscribe()
+    }, [])
+
+    return children
+}
+
+export default AuthWrapper
