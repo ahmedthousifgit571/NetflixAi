4e928dfce1c2a611e755c30b3d0417175f6a9a93
ahmed thousif <ahmedthousif571@gmail.com>
2025-06-23 12:20:34 +0530

    changes

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -2,7 +2,7 @@ import React, {useState } from 'react'
 import Header from './Header' 
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 import MainContainer from './MainContainer';
-import SecondaryContainer from './secondaryContainer';
+import SecondaryContainer from './SecondaryContainer.jsx';
 import usePopularMovies from '../hooks/usePopularMovies';
 import useUpcomingMovies from '../hooks/useUpcomingMovies';
 import useTopRatedMovies from '../hooks/useTopRatedMovies';
